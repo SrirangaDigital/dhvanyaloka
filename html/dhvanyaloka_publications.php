4174d2b8c1b10e0223d1749333f5581c80c97b3e
kishorssk <kishor.sathigrama@gmail.com>
2017-05-15 12:59:06 +0530

    added href links

--- html/dhvanyaloka_publications.php
+++ html/dhvanyaloka_publications.php
@@ -6,34 +6,34 @@ include ('includes/header_inner.php');
 
  <h3>Dhvanyaloka Publications</h3>
  <div class="col-sm-3"><a href="books/001.pdf" target="_blank"><img src="images/coverpage/001.jpg" class="img-responsive img-p" alt=""><span class="book-name">F.R. Leavis Birth Centenary Essays</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/002.jpg" class="img-responsive img-p" alt=""><span class="book-name">Problems Of Translation</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/003.jpg" class="img-responsive img-p" alt=""><span class="book-name">Nobel Laureates In Literature</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/004.jpg" class="img-responsive img-p" alt=""><span class="book-name">Identiti of Text and Reader Response</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/005.jpg" class="img-responsive img-p" alt=""><span class="book-name">Kipling`s India</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/006.jpg" class="img-responsive img-p" alt=""><span class="book-name">Negotiating Differences</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/007.jpg" class="img-responsive img-p" alt=""><span class="book-name">What Have The Arts In Common?</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/008.jpg" class="img-responsive img-p" alt=""><span class="book-name">The Flowering of Australian Literature	</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/009.jpg" class="img-responsive img-p" alt=""><span class="book-name">Fifty Years Of Things Fall Apart</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/010.jpg" class="img-responsive img-p" alt=""><span class="book-name">Commonwealth Autobiography</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/011.jpg" class="img-responsive img-p" alt=""><span class="book-name">African Literature Comes Of Age</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/012.jpg" class="img-responsive img-p" alt=""><span class="book-name">New Directions In African Writing</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/013.jpg" class="img-responsive img-p" alt=""><span class="book-name">Asian Identity In Literature</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/014.jpg" class="img-responsive img-p" alt=""><span class="book-name">New Trends In British Literary Scene</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/015.jpg" class="img-responsive img-p" alt=""><span class="book-name">F.R. Leavis: The Critic As Crusader</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/016.jpg" class="img-responsive img-p" alt=""><span class="book-name">R.K. Narayan Centenary Essays</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/017.jpg" class="img-responsive img-p" alt=""><span class="book-name">Indian Women`s Fiction Today</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/018.jpg" class="img-responsive img-p" alt=""><span class="book-name">Kannada Literary Scene</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/019.jpg" class="img-responsive img-p" alt=""><span class="book-name">Drama As Form Of Art And Theatre</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/020.jpg" class="img-responsive img-p" alt=""><span class="book-name">Contemporary Teulgu Literary Scene</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/021.jpg" class="img-responsive img-p" alt=""><span class="book-name">Great Moments In Life and Literature</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/022.jpg" class="img-responsive img-p" alt=""><span class="book-name">Glimpses of New Zealand Literature</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/023.jpg" class="img-responsive img-p" alt=""><span class="book-name">The Vitality of West Indian Literature</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/024.jpg" class="img-responsive img-p" alt=""><span class="book-name">Western Writers On India</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/025.jpg" class="img-responsive img-p" alt=""><span class="book-name">Common Poetic for Indian Literatures</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/026.jpg" class="img-responsive img-p" alt=""><span class="book-name">Bhakti in Indian Literature</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/027.jpg" class="img-responsive img-p" alt=""><span class="book-name">T S Elliot and Literary Scene</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/028.jpg" class="img-responsive img-p" alt=""><span class="book-name">Indian Literature in English</span></a></div>
- <div class="col-sm-3"><a href="" target="_blank"><img src="images/coverpage/029.jpg" class="img-responsive img-p" alt=""><span class="book-name">Tenth year of Publication</span></a></div>
+ <div class="col-sm-3"><a href="books/002.pdf" target="_blank"><img src="images/coverpage/002.jpg" class="img-responsive img-p" alt=""><span class="book-name">Problems Of Translation</span></a></div>
+ <div class="col-sm-3"><a href="books/003.pdf" target="_blank"><img src="images/coverpage/003.jpg" class="img-responsive img-p" alt=""><span class="book-name">Nobel Laureates In Literature</span></a></div>
+ <div class="col-sm-3"><a href="books/004.pdf" target="_blank"><img src="images/coverpage/004.jpg" class="img-responsive img-p" alt=""><span class="book-name">Identiti of Text and Reader Response</span></a></div>
+ <div class="col-sm-3"><a href="books/005.pdf" target="_blank"><img src="images/coverpage/005.jpg" class="img-responsive img-p" alt=""><span class="book-name">Kipling`s India</span></a></div>
+ <div class="col-sm-3"><a href="books/006.pdf" target="_blank"><img src="images/coverpage/006.jpg" class="img-responsive img-p" alt=""><span class="book-name">Negotiating Differences</span></a></div>
+ <div class="col-sm-3"><a href="books/007.pdf" target="_blank"><img src="images/coverpage/007.jpg" class="img-responsive img-p" alt=""><span class="book-name">What Have The Arts In Common?</span></a></div>
+ <div class="col-sm-3"><a href="books/008.pdf" target="_blank"><img src="images/coverpage/008.jpg" class="img-responsive img-p" alt=""><span class="book-name">The Flowering of Australian Literature	</span></a></div>
+ <div class="col-sm-3"><a href="books/009.pdf" target="_blank"><img src="images/coverpage/009.jpg" class="img-responsive img-p" alt=""><span class="book-name">Fifty Years Of Things Fall Apart</span></a></div>
+ <div class="col-sm-3"><a href="books/010.pdf" target="_blank"><img src="images/coverpage/010.jpg" class="img-responsive img-p" alt=""><span class="book-name">Commonwealth Autobiography</span></a></div>
+ <div class="col-sm-3"><a href="books/011.pdf" target="_blank"><img src="images/coverpage/011.jpg" class="img-responsive img-p" alt=""><span class="book-name">African Literature Comes Of Age</span></a></div>
+ <div class="col-sm-3"><a href="books/012.pdf" target="_blank"><img src="images/coverpage/012.jpg" class="img-responsive img-p" alt=""><span class="book-name">New Directions In African Writing</span></a></div>
+ <div class="col-sm-3"><a href="books/013.pdf" target="_blank"><img src="images/coverpage/013.jpg" class="img-responsive img-p" alt=""><span class="book-name">Asian Identity In Literature</span></a></div>
+ <div class="col-sm-3"><a href="books/014.pdf" target="_blank"><img src="images/coverpage/014.jpg" class="img-responsive img-p" alt=""><span class="book-name">New Trends In British Literary Scene</span></a></div>
+ <div class="col-sm-3"><a href="books/015.pdf" target="_blank"><img src="images/coverpage/015.jpg" class="img-responsive img-p" alt=""><span class="book-name">F.R. Leavis: The Critic As Crusader</span></a></div>
+ <div class="col-sm-3"><a href="books/016.pdf" target="_blank"><img src="images/coverpage/016.jpg" class="img-responsive img-p" alt=""><span class="book-name">R.K. Narayan Centenary Essays</span></a></div>
+ <div class="col-sm-3"><a href="books/017.pdf" target="_blank"><img src="images/coverpage/017.jpg" class="img-responsive img-p" alt=""><span class="book-name">Indian Women`s Fiction Today</span></a></div>
+ <div class="col-sm-3"><a href="books/018.pdf" target="_blank"><img src="images/coverpage/018.jpg" class="img-responsive img-p" alt=""><span class="book-name">Kannada Literary Scene</span></a></div>
+ <div class="col-sm-3"><a href="books/019.pdf" target="_blank"><img src="images/coverpage/019.jpg" class="img-responsive img-p" alt=""><span class="book-name">Drama As Form Of Art And Theatre</span></a></div>
+ <div class="col-sm-3"><a href="books/020.pdf" target="_blank"><img src="images/coverpage/020.jpg" class="img-responsive img-p" alt=""><span class="book-name">Contemporary Teulgu Literary Scene</span></a></div>
+ <div class="col-sm-3"><a href="books/021.pdf" target="_blank"><img src="images/coverpage/021.jpg" class="img-responsive img-p" alt=""><span class="book-name">Great Moments In Life and Literature</span></a></div>
+ <div class="col-sm-3"><a href="books/022.pdf" target="_blank"><img src="images/coverpage/022.jpg" class="img-responsive img-p" alt=""><span class="book-name">Glimpses of New Zealand Literature</span></a></div>
+ <div class="col-sm-3"><a href="books/023.pdf" target="_blank"><img src="images/coverpage/023.jpg" class="img-responsive img-p" alt=""><span class="book-name">The Vitality of West Indian Literature</span></a></div>
+ <div class="col-sm-3"><a href="books/024.pdf" target="_blank"><img src="images/coverpage/024.jpg" class="img-responsive img-p" alt=""><span class="book-name">Western Writers On India</span></a></div>
+ <div class="col-sm-3"><a href="books/025.pdf" target="_blank"><img src="images/coverpage/025.jpg" class="img-responsive img-p" alt=""><span class="book-name">Common Poetic for Indian Literatures</span></a></div>
+ <div class="col-sm-3"><a href="books/026.pdf" target="_blank"><img src="images/coverpage/026.jpg" class="img-responsive img-p" alt=""><span class="book-name">Bhakti in Indian Literature</span></a></div>
+ <div class="col-sm-3"><a href="books/027.pdf" target="_blank"><img src="images/coverpage/027.jpg" class="img-responsive img-p" alt=""><span class="book-name">T S Elliot and Literary Scene</span></a></div>
+ <div class="col-sm-3"><a href="books/028.pdf" target="_blank"><img src="images/coverpage/028.jpg" class="img-responsive img-p" alt=""><span class="book-name">Indian Literature in English</span></a></div>
+ <div class="col-sm-3"><a href="books/029.pdf" target="_blank"><img src="images/coverpage/029.jpg" class="img-responsive img-p" alt=""><span class="book-name">Tenth year of Publication</span></a></div>
 
 
 </div>
