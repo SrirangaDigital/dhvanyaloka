250bf42589a6a81ad15669d0ab1e9cc45a481edc
kishorssk <kishor.sathigrama@gmail.com>
2017-08-24 13:55:52 +0530

    Added literary criterions thumbnails

--- html/the_literary_criterion.php
+++ html/the_literary_criterion.php
@@ -8,6 +8,14 @@ include ('includes/header_inner.php');
 <p>The Literary Criterion  the most influential critical journal in English in India for the past 58 years, apart from generating a critical climate, has introduced American literature, Commonwealth Literature, and promoted Indian writing in English Translation studies and our own ancient traditional critical theories.  Its singular objective is to win attention to deserving literary texts especially by making an Indian approach thereby fusing the classical with the contemporary.</p>
 <p>It has also brought out more than twenty five special issues some of which are still available for sale.</p>
 <p>Annual subscription for the quarterly is – individuals : Rs.500/-, institutions: Rs.800/-; Foreign for both individuals and institutions - $30 + $10 for airmail postage.  Some back numbers are available for sale.</p>
+ <hr>
+ <div class="container">
+ <div class="col-sm-3"><a href="toc.php?bookid=025"><img src="images/coverpage/025.jpg" class="img-responsive img-p" alt=""><span class="book-name">Common Poetic for Indian Literatures</span></a></div>
+ <div class="col-sm-3"><a href="toc.php?bookid=026"><img src="images/coverpage/026.jpg" class="img-responsive img-p" alt=""><span class="book-name">Bhakti in Indian Literature</span></a></div>
+ <div class="col-sm-3"><a href="toc.php?bookid=027"><img src="images/coverpage/027.jpg" class="img-responsive img-p" alt=""><span class="book-name">T S Elliot and Literary Scene</span></a></div>
+ <div class="col-sm-3"><a href="toc.php?bookid=028"><img src="images/coverpage/028.jpg" class="img-responsive img-p" alt=""><span class="book-name">Indian Literature in English</span></a></div>
+ <div class="col-sm-3"><a href="toc.php?bookid=029"><img src="images/coverpage/029.jpg" class="img-responsive img-p" alt=""><span class="book-name">Tenth year of Publication</span></a></div>
+</div>
 <hr>
 <p>All correspondence regarding contribution and subscription with the Editor, </p>
 <p>Dhvanyaloka, Bogadi Road, Mysore 560 006, INDIA </p>
