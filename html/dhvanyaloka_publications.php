e09f7a458e69ecf89ed9d5812bd3a236675ba2a2
kishorssk <kishor.sathigrama@gmail.com>
2017-08-24 14:21:31 +0530

    changes in user interface

--- html/dhvanyaloka_publications.php
+++ html/dhvanyaloka_publications.php
@@ -3,41 +3,40 @@
 include ('includes/header_inner.php');
 ?>
 <div class="container">
-
- <h3>Dhvanyaloka Publications</h3>
- <div class="col-sm-3"><a href="toc.php?bookid=001"><img src="images/coverpage/001.jpg" class="img-responsive img-p" alt=""><span class="book-name">F.R. Leavis Birth Centenary Essays</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=002"><img src="images/coverpage/002.jpg" class="img-responsive img-p" alt=""><span class="book-name">Problems Of Translation</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=003"><img src="images/coverpage/003.jpg" class="img-responsive img-p" alt=""><span class="book-name">Nobel Laureates In Literature</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=004"><img src="images/coverpage/004.jpg" class="img-responsive img-p" alt=""><span class="book-name">Identiti of Text and Reader Response</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=005"><img src="images/coverpage/005.jpg" class="img-responsive img-p" alt=""><span class="book-name">Kipling`s India</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=006"><img src="images/coverpage/006.jpg" class="img-responsive img-p" alt=""><span class="book-name">Negotiating Differences</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=007"><img src="images/coverpage/007.jpg" class="img-responsive img-p" alt=""><span class="book-name">What Have The Arts In Common?</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=008"><img src="images/coverpage/008.jpg" class="img-responsive img-p" alt=""><span class="book-name">The Flowering of Australian Literature	</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=009"><img src="images/coverpage/009.jpg" class="img-responsive img-p" alt=""><span class="book-name">Fifty Years Of Things Fall Apart</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=010"><img src="images/coverpage/010.jpg" class="img-responsive img-p" alt=""><span class="book-name">Commonwealth Autobiography</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=011"><img src="images/coverpage/011.jpg" class="img-responsive img-p" alt=""><span class="book-name">African Literature Comes Of Age</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=012"><img src="images/coverpage/012.jpg" class="img-responsive img-p" alt=""><span class="book-name">New Directions In African Writing</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=013"><img src="images/coverpage/013.jpg" class="img-responsive img-p" alt=""><span class="book-name">Asian Identity In Literature</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=014"><img src="images/coverpage/014.jpg" class="img-responsive img-p" alt=""><span class="book-name">New Trends In British Literary Scene</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=015"><img src="images/coverpage/015.jpg" class="img-responsive img-p" alt=""><span class="book-name">F.R. Leavis: The Critic As Crusader</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=016"><img src="images/coverpage/016.jpg" class="img-responsive img-p" alt=""><span class="book-name">R.K. Narayan Centenary Essays</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=017"><img src="images/coverpage/017.jpg" class="img-responsive img-p" alt=""><span class="book-name">Indian Women`s Fiction Today</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=018"><img src="images/coverpage/018.jpg" class="img-responsive img-p" alt=""><span class="book-name">Kannada Literary Scene</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=019"><img src="images/coverpage/019.jpg" class="img-responsive img-p" alt=""><span class="book-name">Drama As Form Of Art And Theatre</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=020"><img src="images/coverpage/020.jpg" class="img-responsive img-p" alt=""><span class="book-name">Contemporary Telugu Literary Scene</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=021"><img src="images/coverpage/021.jpg" class="img-responsive img-p" alt=""><span class="book-name">Great Moments In Life and Literature</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=022"><img src="images/coverpage/022.jpg" class="img-responsive img-p" alt=""><span class="book-name">Glimpses of New Zealand Literature</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=023"><img src="images/coverpage/023.jpg" class="img-responsive img-p" alt=""><span class="book-name">The Vitality of West Indian Literature</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=024"><img src="images/coverpage/024.jpg" class="img-responsive img-p" alt=""><span class="book-name">Western Writers On India</span></a></div>
-
-
-</div>
-<div class="container">
-	<hr>
-<p>All correspondence regarding contribution and subscription with the Editor, </p>
-<p>Dhvanyaloka, Bogadi Road, Mysore 560 006, INDIA </p>
-<p>Email: srinathcn@rediffmail.com</p>
-<p>Mobile : 91 99724 15369</p>
+	<div class="row">
+	 <h3>Dhvanyaloka Publications</h3>
+	 <div class="col-sm-3"><a href="toc.php?bookid=001"><img src="images/coverpage/001.jpg" class="img-responsive img-p" alt=""><span class="book-name">F.R. Leavis Birth Centenary Essays</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=002"><img src="images/coverpage/002.jpg" class="img-responsive img-p" alt=""><span class="book-name">Problems Of Translation</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=003"><img src="images/coverpage/003.jpg" class="img-responsive img-p" alt=""><span class="book-name">Nobel Laureates In Literature</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=004"><img src="images/coverpage/004.jpg" class="img-responsive img-p" alt=""><span class="book-name">Identiti of Text and Reader Response</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=005"><img src="images/coverpage/005.jpg" class="img-responsive img-p" alt=""><span class="book-name">Kipling`s India</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=006"><img src="images/coverpage/006.jpg" class="img-responsive img-p" alt=""><span class="book-name">Negotiating Differences</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=007"><img src="images/coverpage/007.jpg" class="img-responsive img-p" alt=""><span class="book-name">What Have The Arts In Common?</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=008"><img src="images/coverpage/008.jpg" class="img-responsive img-p" alt=""><span class="book-name">The Flowering of Australian Literature	</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=009"><img src="images/coverpage/009.jpg" class="img-responsive img-p" alt=""><span class="book-name">Fifty Years Of Things Fall Apart</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=010"><img src="images/coverpage/010.jpg" class="img-responsive img-p" alt=""><span class="book-name">Commonwealth Autobiography</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=011"><img src="images/coverpage/011.jpg" class="img-responsive img-p" alt=""><span class="book-name">African Literature Comes Of Age</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=012"><img src="images/coverpage/012.jpg" class="img-responsive img-p" alt=""><span class="book-name">New Directions In African Writing</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=013"><img src="images/coverpage/013.jpg" class="img-responsive img-p" alt=""><span class="book-name">Asian Identity In Literature</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=014"><img src="images/coverpage/014.jpg" class="img-responsive img-p" alt=""><span class="book-name">New Trends In British Literary Scene</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=015"><img src="images/coverpage/015.jpg" class="img-responsive img-p" alt=""><span class="book-name">F.R. Leavis: The Critic As Crusader</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=016"><img src="images/coverpage/016.jpg" class="img-responsive img-p" alt=""><span class="book-name">R.K. Narayan Centenary Essays</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=017"><img src="images/coverpage/017.jpg" class="img-responsive img-p" alt=""><span class="book-name">Indian Women`s Fiction Today</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=018"><img src="images/coverpage/018.jpg" class="img-responsive img-p" alt=""><span class="book-name">Kannada Literary Scene</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=019"><img src="images/coverpage/019.jpg" class="img-responsive img-p" alt=""><span class="book-name">Drama As Form Of Art And Theatre</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=020"><img src="images/coverpage/020.jpg" class="img-responsive img-p" alt=""><span class="book-name">Contemporary Telugu Literary Scene</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=021"><img src="images/coverpage/021.jpg" class="img-responsive img-p" alt=""><span class="book-name">Great Moments In Life and Literature</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=022"><img src="images/coverpage/022.jpg" class="img-responsive img-p" alt=""><span class="book-name">Glimpses of New Zealand Literature</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=023"><img src="images/coverpage/023.jpg" class="img-responsive img-p" alt=""><span class="book-name">The Vitality of West Indian Literature</span></a></div>
+	 <div class="col-sm-3"><a href="toc.php?bookid=024"><img src="images/coverpage/024.jpg" class="img-responsive img-p" alt=""><span class="book-name">Western Writers On India</span></a></div>
+	</div>
+	<div class="row">
+		<hr>
+	<p>All correspondence regarding contribution and subscription with the Editor, </p>
+	<p>Dhvanyaloka, Bogadi Road, Mysore 560 006, INDIA </p>
+	<p>Email: srinathcn@rediffmail.com</p>
+	<p>Mobile : 91 99724 15369</p>
+	</div>
 </div>
 <?php
 include ('includes/footer.php');
--- html/the_literary_criterion.php
+++ html/the_literary_criterion.php
@@ -4,24 +4,26 @@ include ('includes/header_inner.php');
 ?>
 <div class="container">
 	<div class="row">
-  <h3>The Literary Criterion</h3>
-<p>The Literary Criterion  the most influential critical journal in English in India for the past 58 years, apart from generating a critical climate, has introduced American literature, Commonwealth Literature, and promoted Indian writing in English Translation studies and our own ancient traditional critical theories.  Its singular objective is to win attention to deserving literary texts especially by making an Indian approach thereby fusing the classical with the contemporary.</p>
-<p>It has also brought out more than twenty five special issues some of which are still available for sale.</p>
-<p>Annual subscription for the quarterly is – individuals : Rs.500/-, institutions: Rs.800/-; Foreign for both individuals and institutions - $30 + $10 for airmail postage.  Some back numbers are available for sale.</p>
- <hr>
- <div class="container">
- <div class="col-sm-3"><a href="toc.php?bookid=025"><img src="images/coverpage/025.jpg" class="img-responsive img-p" alt=""><span class="book-name">Common Poetic for Indian Literatures</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=026"><img src="images/coverpage/026.jpg" class="img-responsive img-p" alt=""><span class="book-name">Bhakti in Indian Literature</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=027"><img src="images/coverpage/027.jpg" class="img-responsive img-p" alt=""><span class="book-name">T S Elliot and Literary Scene</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=028"><img src="images/coverpage/028.jpg" class="img-responsive img-p" alt=""><span class="book-name">Indian Literature in English</span></a></div>
- <div class="col-sm-3"><a href="toc.php?bookid=029"><img src="images/coverpage/029.jpg" class="img-responsive img-p" alt=""><span class="book-name">Tenth year of Publication</span></a></div>
-</div>
+	<h3>The Literary Criterion</h3>
+	<p>The Literary Criterion  the most influential critical journal in English in India for the past 58 years, apart from generating a critical climate, has introduced American literature, Commonwealth Literature, and promoted Indian writing in English Translation studies and our own ancient traditional critical theories.  Its singular objective is to win attention to deserving literary texts especially by making an Indian approach thereby fusing the classical with the contemporary.</p><br>
+	<p>It has also brought out more than twenty five special issues some of which are still available for sale.</p><br>
+	<p>Annual subscription for the quarterly is – individuals : Rs.500/-, institutions: Rs.800/-; Foreign for both individuals and institutions - $30 + $10 for airmail postage.  Some back numbers are available for sale.</p>
+	 <hr>
+	</div>
+	<div class="row">
+		 <div class="col-sm-3"><a href="toc.php?bookid=025"><img src="images/coverpage/025.jpg" class="img-responsive img-p" alt=""><span class="book-name">Common Poetic for Indian Literatures</span></a></div>
+		 <div class="col-sm-3"><a href="toc.php?bookid=026"><img src="images/coverpage/026.jpg" class="img-responsive img-p" alt=""><span class="book-name">Bhakti in Indian Literature</span></a></div>
+		 <div class="col-sm-3"><a href="toc.php?bookid=027"><img src="images/coverpage/027.jpg" class="img-responsive img-p" alt=""><span class="book-name">T S Elliot and Literary Scene</span></a></div>
+		 <div class="col-sm-3"><a href="toc.php?bookid=028"><img src="images/coverpage/028.jpg" class="img-responsive img-p" alt=""><span class="book-name">Indian Literature in English</span></a></div>
+		 <div class="col-sm-3"><a href="toc.php?bookid=029"><img src="images/coverpage/029.jpg" class="img-responsive img-p" alt=""><span class="book-name">Tenth year of Publication</span></a></div>
+	</div>
 <hr>
-<p>All correspondence regarding contribution and subscription with the Editor, </p>
-<p>Dhvanyaloka, Bogadi Road, Mysore 560 006, INDIA </p>
-<p>Email: srinathcn@rediffmail.com</p>
-<p>Mobile : 91 99724 15369</p>
-</div>
+	<div class="row">
+	<p>All correspondence regarding contribution and subscription with the Editor, </p>
+	<p>Dhvanyaloka, Bogadi Road, Mysore 560 006, INDIA </p>
+	<p>Email: srinathcn@rediffmail.com</p>
+	<p>Mobile : 91 99724 15369</p>
+	</div>
 </div>	
 <?php
 include ('includes/footer.php');
